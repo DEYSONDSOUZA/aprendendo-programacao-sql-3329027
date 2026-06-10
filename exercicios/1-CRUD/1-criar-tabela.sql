2d0c3372889d1f0d4ccbbbdefe0672a24aea320f
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-10 19:19:35 -0400

    adicao de mais tabelas

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -5,3 +5,14 @@ CREATE TABLE clientes (
     email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(20)
 );
+create table salarios (
+    id integer notnull,
+    cargo varchar(15) notnull,
+    salarios integer notnull
+);
+CREATE TABLE remuneracao (
+    id CPF PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(20) NOT NULL,
+    salario INTEGER NOT NULL
+);
