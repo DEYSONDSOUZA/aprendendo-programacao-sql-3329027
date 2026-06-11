ccda1d0526b2d0b8e1af4cce6b45c4b6bd564c4d
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-10 20:46:56 -0400

    inseri classes (dados) na tabela salario_classe

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,11 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe(id, nivel, salario)
+VALUES 
+  (1, 'Estagio' , 2150),
+  (2, 'Analista TI' , 3500),
+  (3, 'Dev front-end jr.' , 5700);
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe(nivel, salario)
+VALUES
+  ('Dev full-stack jr.', 8000)
