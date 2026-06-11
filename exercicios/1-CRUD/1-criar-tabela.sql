f10bd6add3048faa63dcf4eb049cb5fb95ec37bf
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-10 20:12:08 -0400

    .

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -5,14 +5,14 @@ CREATE TABLE clientes (
     email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(20)
 );
-create table salarios (
-    id integer notnull,
-    cargo varchar(15) notnull,
-    salarios integer notnull
-);
 CREATE TABLE remuneracao (
     id CPF PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cargo VARCHAR(20) NOT NULL,
     salario INTEGER NOT NULL
+);
+CREATE TABLE salario_classe (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nivel TEXT,
+    salario INTEGER
 );
