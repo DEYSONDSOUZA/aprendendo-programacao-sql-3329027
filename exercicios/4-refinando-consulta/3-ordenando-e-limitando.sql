d16dfa9f87ecad3bf3bfd7e72adea7fa7c69c740
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-17 17:40:06 -0400

    adiciona consulta para retornar 10 clientes ordenados por país e nome

--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,13 @@
 -- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
-
-
 -- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
-
-
 -- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+SELECT 
+  CustomerID,
+  FirstName,
+  Country
+FROM
+  customers
+ORDER BY
+  Country ASC,
+  FirstName DESC
+LIMIT 10
