42ca7d34b3580bedc7f2ca39f68cf28777dee3ec
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-17 11:13:06 -0400

    adiciona consulta para retornar colunas específicas da tabela country

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,12 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT
+    country,
+    State,
+    CASE 
+        WHEN state = 'SP' THEN 'São Paulo'
+        WHEN state = 'RJ' THEN 'Rio de Janeiro'
+        WHEN state = 'AM' THEN 'Amazonas'
+        ELSE state
+    END AS Estado
+FROM customers
+WHERE country = 'Brazil';
