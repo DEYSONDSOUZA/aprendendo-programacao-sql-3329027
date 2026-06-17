e6d38242c4ca7369f0eb6047a3d379c1366a28e3
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-17 16:28:07 -0400

    refatora consulta para utilizar a cláusula "with"  mas nao obtive sucesso com a criacao dessa tabela temporaria

--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,19 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
-
-
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+WITH musicas AS (
+SELECT
+  tra.TrackId AS Id,
+  tra.Name AS NomeMusica,
+  alb.Title AS Album,
+  art.Name AS Artista
+FROM
+  tracks as tra
+  INNER JOIN albums AS alb ON alb.AlbumId = tra.AlbumId
+  INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId
+)
+
+SELECT
+  Artista,
+  COUNT(musicas) AS TotalMusicas
+FROM musicas
+WHERE Artista = 'Caetano Veloso'
