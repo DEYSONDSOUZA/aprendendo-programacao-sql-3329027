42578d130efd2ad704d4dba3474c5b8b6f726c21
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-17 17:18:09 -0400

    adiciona consulta para calcular total de gastos por cliente filtrando aqueles que gastaram mais de R$30 e inclui a compra de maior valor

--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,14 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
-
-
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
-
-
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+SELECT 
+  cus.CustomerId AS id,
+  cus.firstname AS nome,
+  SUM(inv.total) AS total_gasto
+FROM
+  invoices AS inv
+  INNER JOIN customers AS cus ON cus.CustomerId = inv.CustomerId
+GROUP BY
+  cus.CustomerId, cus.firstname
+HAVING
+  SUM(inv.total) > 30
