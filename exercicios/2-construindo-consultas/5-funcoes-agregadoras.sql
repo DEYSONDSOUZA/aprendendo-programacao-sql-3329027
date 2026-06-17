653671406c955aa13a92187085bb60b9a460c872
DEYSONDSOUZA <deysonnsouza@gmail.com>
2026-06-17 15:37:47 -0400

    adiciona consulta para calcular soma, mínimo, máximo, quantidade e ticket médio de compras por país na tabela invoices

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,1 +1,10 @@
 -- Para a tabela "invoices", crie uma consulta que retorne o país e a soma das compras realizadas nele, bem como o valor da compra mínima e máxima, a quantidade de compras realizadas e o ticket médio de cada compra.
+SELECT
+  BillingCountry AS PaisDeCompra,
+  SUM(Total) AS SomaCompras,
+  MIN(Total) AS CompraMinima,
+  MAX(Total) AS CompraMaxima,
+  COUNT(Total) AS QuantidadeCompras,
+  ROUND(AVG(Total), 2) AS TicketMedio
+FROM invoices
+GROUP BY BillingCountry;
